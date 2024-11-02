9391bfeaab6d9b0e893f19f38f1f4337624ab7b5
anhhong225 <anhhong225@gmail.com>
2024-11-02 13:25:05 +0700

    change structure of file

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const userRoutes = require('./routes/userRoutes');
+const wasteCategoryRoutes = require('./routes/wasteCategoryRoutes');
+const wasteItemRoutes = require('./routes/wasteItemRoutes');
+const challengeRoutes = require('./routes/challengeRoutes');
+const errorMiddleware = require('./middlewares/errorMiddleware');
 require('dotenv').config();
 
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.get('/', (req, res) => {
-    res.status(200).send('User registration');
-});
+app.use(express.json());
 
-app.listen(port, () =>{
+// Route setup
+app.use('/api/users', userRoutes);
+app.use('/api/waste-categories', wasteCategoryRoutes);
+app.use('/api/waste-items', wasteItemRoutes);
+app.use('/api/challenges', challengeRoutes);
+
+// Error handling middleware
+app.use(errorMiddleware);
+
+app.listen(PORT, () =>{
     console.log("Server is running on port 3000");
 });
