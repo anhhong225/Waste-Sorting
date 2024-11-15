f68c73da623ea7dfce459b1d66fa11a2016ceb06
Bùi Trung Hiếu <134358078+thiu123@users.noreply.github.com>
2024-11-15 16:43:18 +0700

    fix route and controller, edit app.status in app

--- waste_api/app.js
+++ waste_api/app.js
@@ -7,7 +7,7 @@ global.wasteCategory = require('./models/wasteCategory');
 global.wasteItem = require('./models/wasteItem');
 
 // Routes
-const wasteCategoryRoutes = require('./routes/wasteCategorieRoute');
+const wasteCategoryRoutes = require('./routes/wasteCategoryRoute');
 const wasteItemRoutes = require('./routes/wasteItemRoute');
 
 mongoose.Promise = global.Promise;
@@ -24,8 +24,8 @@ wasteCategoryRoutes(app);
 wasteItemRoutes(app);
 
 // Start the server
-app.use((req,res) => {
-    req.statusCode(404).send({url: `${req.originalUrl} not found`});
+app.use((req, res) => {
+  res.status(404).send({url: `${req.originalUrl} not found`});
 });
 
 app.listen(PORT, () => {
--- waste_api/controllers/wasteItemController.js
+++ waste_api/controllers/wasteItemController.js
@@ -1,49 +1,67 @@
-const wasteItem = require('../models/wasteItem');
-const mongoose = require('mongoose');
+const WasteItem = require("../models/wasteItem"); 
+// const mongoose = require("mongoose");
 
-//get all waste categories
-exports.getItems = (req,res) => {
-    WasteItem.find({}, (err, wasteItems) => {
-        if(err) res.send(err);
-        res.json(wasteItems);
-    });
+// Get all waste items
+exports.getItems = (req, res) => {
+  WasteItem.find({}, (err, wasteItems) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    res.json(wasteItems);
+  });
 };
-//Get a waste category by Id
+
+// Get a waste item by Id
 exports.getAItem = (req, res) => {
-    WasteItem.findById(req.params.itemId, (err, wasteItem) =>{
-        if(err) res.send(err);
-        res.json(wasteItem);
-    });
+  WasteItem.findById(req.params.itemId, (err, wasteItem) => {
+    if (err) {
+      return res.status(500).send(err); // Handling server error
+    }
+    if (!wasteItem) {
+      return res.status(404).json({ message: "Waste item not found" }); // If the item is not found
+    }
+    res.json(wasteItem);
+  });
 };
-// Create a new waste category
+
+// Create a new waste item
 exports.createItem = (req, res) => {
-    const newWasteItem = new WasteItem(req.body);
-    newWasteItem.save((err, wasteItem) => {
-        if (err) res.send(err);
-        res.json(wasteItem);
-    });
+  const newWasteItem = new WasteItem(req.body);
+  newWasteItem.save((err, wasteItem) => {
+    if (err) {
+      return res.status(500).send(err); // Handling server error
+    }
+    res.status(201).json(wasteItem); // Send back created item with status 201
+  });
 };
 
-// Update a waste category
+// Update a waste item
 exports.updateItem = (req, res) => {
-    WasteItem.findOneAndUpdate(
-        { _id: req.params.itemId },
-        req.body,
-        { new: true },
-        (err, wasteItem) => {
-            if (err) res.send(err);
-            res.json(wasteItem);
-        }
-    );
+  WasteItem.findOneAndUpdate(
+    { _id: req.params.itemId },
+    req.body,
+    { new: true }, // Return the updated document
+    (err, wasteItem) => {
+      if (err) {
+        return res.status(500).send(err); // Handling server error
+      }
+      if (!wasteItem) {
+        return res.status(404).json({ message: "Waste item not found" }); // If item is not found
+      }
+      res.json(wasteItem);
+    }
+  );
 };
 
-// Delete a waste category
+// Delete a waste item
 exports.deleteItem = (req, res) => {
-    WasteItem.deleteOne({ _id: req.params.itemId }, err => {
-        if (err) res.send(err);
-        res.json({
-            message: 'Waste item successfully deleted',
-            itemId: req.params.itemId
-        });
+  WasteItem.deleteOne({ _id: req.params.itemId }, (err) => {
+    if (err) {
+      return res.status(500).send(err); // Handling server error
+    }
+    res.json({
+      message: "Waste item successfully deleted",
+      itemId: req.params.itemId,
     });
+  });
 };
