953d236b617bee29deb24823fd734251e1959218
Linh <volinhrgkg@gmail.com>
2024-11-15 15:00:27 +0700

    Add challenge

--- user_api/app.js
+++ user_api/app.js
@@ -10,7 +10,7 @@ global.Challenge = require('./models/challenge');
 
 // Routes
 const userRoutes = require('./routes/userRoute');
-const challengeRoutes = require('./routes/challengeRoute');
+// const challengeRoutes = require('./routes/challengeRoute');
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb+srv://heoshan:Gemini2205@waste-sorting.iibpb.mongodb.net/?retryWrites=true&w=majority&appName=Waste-Sorting');
@@ -19,15 +19,15 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 userRoutes(app);
-challengeRoutes(app);
+// challengeRoutes(app);
 
 // Start the server
-app.use((req,res) => {
-    req.statusCode(404).send({url: `${req.originalUrl} not found`});
+app.use((req, res) => {
+  req.statusCode(404).send({ url: `${req.originalUrl} not found` });
 });
 
 app.listen(PORT, () => {
